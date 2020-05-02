bd45e2c67920f5d1d411e6c5dc73b4629cd7d0ed
Jordan Huus <jordanhuusi@gmail.com>
2020-05-02 13:50:01 -0700

    Migrated file structure.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# TODO(jordanhuus): remove when productionizing the app
-cd backend
-export DATABASE_URL=postgresql://jordanhuus@localhost:5432/capstone_sudoku_solver
-
 # Run the application
-gunicorn app:app
+cd backend/src/
+gunicorn "sudoku_app:create_app()"
