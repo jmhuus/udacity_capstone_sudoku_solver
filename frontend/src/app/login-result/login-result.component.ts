7f6af0b01486bc3af691ceea1bf77a45f115c64e
Jordan Huus <jordanhuusi@gmail.com>
2020-05-03 18:31:34 -0700

    Added login-result component.

---
+++ frontend/src/app/login-result/login-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-result',
+  templateUrl: './login-result.component.html',
+  styleUrls: ['./login-result.component.scss']
+})
+export class LoginResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
